955960a3d80541aaa6c10386cbac58f3879f8283
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-23 21:50:17 +0300

    commit tree 403ae25325b6e2dafb4d862a58fe281d35083468

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,8 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name, :id, :grade;
+  attr_accessor :name,  :grade;
+  attr_reader :id
 
   def initialize (id:nil ,name:, grade:) 
     @id =id
@@ -42,9 +43,9 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
+
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-      self
     end
   end
 
